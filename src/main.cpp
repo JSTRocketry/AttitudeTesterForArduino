ece123101e1cd4fedc5e7c1238e21cddf8303c79
Jordan <jstrocketry@gmail.com>
2018-07-28 21:44:22 -0400

    arudino code for false altitude tester

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,93 @@
 #include <Arduino.h>
-#define END_LINE "b'\\r\\n'"
 
 String X_SYNTAX = "X:";
 String Y_SYNTAX = "Y:";
 String Z_SYNTAX = "Z:";
-char data[100];
+String ALTITUDE_SYNTAX = "PA:";
 int pos = 0;
+char convertedData[100];
 float xData = -1;
 float yData = -1;
 float zData = -1;
-float tsData;
+float altData = -1;
+float tsData = -1;
 bool compiled = false;
+long startTime = 0;
+
+class DataParser {
+    int syntaxPosition = 0;
+    float parseData(String data){
+      int syntax = getSyntax(data);
+      switch (syntax) {
+        case 0:
+          syntaxPosition = data.indexOf(X_SYNTAX+X_SYNTAX.length());
+          return data.substring(syntaxPosition).toFloat();
+        case 1:
+          syntaxPosition = data.indexOf(Y_SYNTAX+Y_SYNTAX.length());
+          return data.substring(syntaxPosition).toFloat();
+        case 2:
+          syntaxPosition = data.indexOf(Z_SYNTAX+Z_SYNTAX.length());
+          return data.substring(syntaxPosition).toFloat();
+        case 3:
+          syntaxPosition = data.indexOf(ALTITUDE_SYNTAX+ALTITUDE_SYNTAX.length());
+          return data.substring(syntaxPosition).toFloat();
+        default:
+          return -1;
+      }
+    }
+
+    int getSyntax(String data){
+      if(data.indexOf(X_SYNTAX) != -1) return 0;
+      else if(data.indexOf(Y_SYNTAX) != -1) return 1;
+      else if(data.indexOf(Z_SYNTAX) != -1) return 2;
+      else if(data.indexOf(ALTITUDE_SYNTAX) != -1) return 3;
+      else return -1;
+    }
+
+  public:
+    DataParser(){}
+    void addData(String data){
+      int syntax = getSyntax(data);
+      switch(syntax){
+        case 0:
+          xData = parseData(data);
+          tsData = millis()-startTime;
+          break;
+        case 1:
+          yData = parseData(data);
+          tsData = millis()-startTime;
+          break;
+        case 2:
+          zData = parseData(data);
+          tsData = millis()-startTime;
+          break;
+        case 3:
+          altData = parseData(data);
+          tsData = millis()-startTime;
+        default:
+          break;
+      }
+    }
+    void sendData(){
+      String toSend = "";
+      if(xData != -1 && yData != -1 && zData != -1 && tsData != -1){
+        toSend = "@{OX:" + String(xData) + ";OY:" + String(yData) + ";OZ:" + String(zData) + ";TS:" + String(tsData) + ";}@";
+        xData, yData, zData, tsData = -1;
+      } else if(altData != -1 && tsData != -1){
+        toSend = "@{PA:" + String(altData) + ";TS:" + String(tsData) + ";}@";
+        altData, tsData = -1;
+      }
+      if(toSend != "") Serial.println(toSend);
+    }
+};
+
+DataParser parser;
 
 void setup() {
   Serial.begin(115200);
-  // long startTime = millis();
-  // data = Serial.readString();
-  // while(Serial.available() > 0){
-  //   if(data.indexOf(X_SYNTAX) >= 0){
-  //     pos = data.indexOf(X_SYNTAX+X_SYNTAX.length());
-  //     xData = data.substring(pos).toFloat();
-  //   } else if(data.indexOf(Y_SYNTAX) >= 0){
-  //     pos = data.indexOf(Y_SYNTAX+Y_SYNTAX.length());
-  //     yData = data.substring(pos).toFloat();
-  //   } else if(data.indexOf(Z_SYNTAX) >= 0){
-  //     pos = data.indexOf(Z_SYNTAX+Z_SYNTAX.length());
-  //     zData = data.substring(pos).toFloat();
-  //   }
+  startTime = millis();
+  parser = DataParser();
+
   //   if(xData != -1 && yData != -1 && zData != -1){
   //     tsData = millis();
   //     data = ("@{OX:%s;OY:%s;OZ:%s;TS:%s;}@", xData, yData, zData, tsData);
@@ -40,13 +101,12 @@ void loop() {
   if(Serial.available() > 0){
     compiled = false;
     int recv = Serial.read();
-    data[pos] = recv;
-    Serial.println(data[pos]);
+    convertedData[pos] = recv;
     pos++;
   }
   else if(pos > 0 && !compiled){
-    String out = data;
-    Serial.println(out);
+    String compiledData = convertedData;
+    parser.addData(compiledData);
     compiled = true;
   }
 }
